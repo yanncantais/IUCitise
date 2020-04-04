e0484bf30983f2a337714b17ce737c945ed68bb0
maximeperrindev <63123596+maximeperrindev@users.noreply.github.com>
2020-04-04 14:19:58 +0200

    Modification de la version de 1.2.1 à 1.2.0

--- WindowsFormsApp4/accueil.cs
+++ WindowsFormsApp4/accueil.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp4
         public accueil()
         {
             InitializeComponent();
-            Properties.Settings.Default.version = "1.2.1";
+            Properties.Settings.Default.version = "1.2.0";
             try
             {
                 con.Open();
