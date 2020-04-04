272cf7969fdef6924c24ab4f5b027ac2a0adc8a1
maximeperrindev <63123596+maximeperrindev@users.noreply.github.com>
2020-04-04 12:18:24 +0200

    Modification de IUcitise en IUCitise sur le titre de l'écran d'accueil

--- WindowsFormsApp4/accueil.Designer.cs
+++ WindowsFormsApp4/accueil.Designer.cs
@@ -51,7 +51,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(221, 78);
             this.label1.TabIndex = 1;
-            this.label1.Text = "IUcitise";
+            this.label1.Text = "IUCitise";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.UseCompatibleTextRendering = true;
             this.label1.Click += new System.EventHandler(this.label1_Click);
